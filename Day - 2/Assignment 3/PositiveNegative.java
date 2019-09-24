1f4a2bfca79a709bb2d3c4ff197bfd8a88598d55
rnv19 <routulasundeep@msitprogram.net>
2019-09-24 18:14:28 +0530

    Assignment 3 Final

--- Day - 2/Assignment 3/PositiveNegative.java
+++ Day - 2/Assignment 3/PositiveNegative.java
@@ -25,6 +25,15 @@
      */
     public static boolean posNeg(int a, int b, boolean negative) {
         // Your code goes here....
-        
+        if (negative){
+            if ((a<0) && (b<0)){
+                return true;
+            }
+            return false;
+        }
+        else if((a<0 || b<0) && (a>0 || b>0)){
+            return true;
+        }
+        return false;
     }
  }
