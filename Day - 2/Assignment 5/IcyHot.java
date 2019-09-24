fadf727b35eefefc71784b1ffae54ab816ee3c7e
rnv19 <routulasundeep@msitprogram.net>
2019-09-24 18:34:50 +0530

    Assignment 5 Final

--- Day - 2/Assignment 5/IcyHot.java
+++ Day - 2/Assignment 5/IcyHot.java
@@ -28,5 +28,9 @@
      */
     public static boolean icyHot(int temp1, int temp2) {
         // Your code goes here...
+        if ((temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0)){
+            return true;
+        } 
+        return false;
     }
 }
