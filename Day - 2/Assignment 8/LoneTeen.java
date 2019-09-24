45d6465221c2afc479d7b6d1a36c0c4ed4d6bbc8
rnv19 <routulasundeep@msitprogram.net>
2019-09-24 19:57:00 +0530

    Assignment 8 Final

--- Day - 2/Assignment 8/LoneTeen.java
+++ Day - 2/Assignment 8/LoneTeen.java
@@ -27,5 +27,8 @@ public class LoneTeen {
      */
     public static boolean loneTeen(int a, int b) {
         // Your code goes here....
+        if ((a >= 13 && a <= 19) && (b >= 13 && b <= 19)) return false;
+        else if (!(a >= 13 && a <= 19) && !(b >= 13 && b <= 19)) return false;
+        else return true;
     }
 }
