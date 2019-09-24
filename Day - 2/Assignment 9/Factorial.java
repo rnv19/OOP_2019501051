0e3a66a8be09dfd8fd3e8ed7f33e473d069f150a
rnv19 <routulasundeep@msitprogram.net>
2019-09-24 17:30:56 +0530

    Assignments

---
+++ Day - 2/Assignment 9/Factorial.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+ public class Factorial {
+
+    /**
+     * This method finds the factorial of a value n. If n is in the range of 
+     * 1 to 20 (inclusive) and retuns the factorial otherwise -1
+     * @param n, the integer n.
+     * @return factorial of n if n is in 1 to 20 (inclusive) and -1 otherwise.
+     */
+    public static long factorial(int n) {
+        return -1;
+    }
+
+ }
