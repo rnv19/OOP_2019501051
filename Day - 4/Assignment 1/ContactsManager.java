0fa1cb166a48db2a144442a8b731314cc220617c
rnv19 <routulasundeep@msitprogram.net>
2019-09-27 15:43:25 +0530

    Assignment 1 fina

--- Day - 4/Assignment 1/ContactsManager.java
+++ Day - 4/Assignment 1/ContactsManager.java
@@ -1,81 +1,82 @@
 /**
  * This class maintains the contacts of your friends / relations / wellwishers
- * 
+ *
  * et.
- * 
+ *
  * The following are the opertaions that this ContactManager application can
  * perform.
  *  an create a contact of your choice
- *  Creates a contact and k the ContactManager list. You can sea ct based on 
+ *  Creates a contact and k the ContactManager list. You can sea ct based on
  * he name in the ContactManager list. You can search fo contact by email. Se
  * rch for a contact  You can delete a contact by name. Search for a contact b
- * sed o the contact and delete from the C 
+ * sed o the contact and delete from the C
  *  or Siva Sankar
  */
 
-class ContactsManager {
+final class ContactsManager {
 
     /**
-     * Define a variable myFriends which is an array that can hold 
+     * Define a variable myFriends which is an array that can hold
      * the contacts of your friends.
      */
-    Contact[] myFriends;
+    private Contact[] myFriends;
     /**
-     * Define a variable friendsCount of type int maintain the number of 
+     * Define a variable friendsCount of type int maintain the number of
      * contacts of your friends.
      */
-    int friendsCount;
+    private int friendsCount;
 
     /**
-     * When the object of ContactManager is created, the constructor should 
+     * When the object of ContactManager is created, the constructor should
      * constrcut the object initialy with the following requirements.
-     * 
+     *
      * 1. Initializes the friendsCount to 0 as there no contacts in the list.
      * 2. myFriends with array initial size of 500
      */
-    public ContactsManager(){
-        int x = 500;
+    private ContactsManager() {
+        final int x = 500;
         this.friendsCount = 0;
-        this.myFriends = new Contact[x]; 
+        this.myFriends = new Contact[x];
     }
 
     /**
-     * The addContact method takes in a parameter of type contact object and 
+     * The addContact method takes in a parameter of type contact object and
      * adds this to the contact list.
-     * @param contact, to be added.
+     * @param contact to be added.
      */
-    public void addContact(Contact contact) {
+    public void addContact(final Contact contact) {
         //  Your code goes here....
         myFriends[friendsCount++] = contact;
 
         //  Nothing to be returned... As this method is void...
     }
- 
     /**
-     * This method search for the contact based on the name of your friend and gives the
-     * first occurance of the contact in your friends list.
-     * @param searchName, the name of the contact to be searched in the friends list.
+     * This method search for the contact based on the name of your friend
+     * and gives the first occurance of the contact in your friends list.
+     * @param searchName     the name of the contact to be searched in
+     * the friends list.
      * @return the contact of the friend, null otherwise.
      */
-    public Contact searchContact(String searchName) {
+    public Contact searchContact(final String searchName) {
         //  Your code goes here....
-        for (int i=0; i< friendsCount; i++){
-            if (myFriends[i].getName().equals(searchName)){
+        for (int i = 0; i < friendsCount; i++) {
+            if (myFriends[i].getName().equals(searchName)) {
                 return myFriends[i];
             }
         }
         return null;
     }
     /**
-     * This method search for the contact based on the email of your friend 
+     * This method search for the contact based on the email of your friend
      * and gives the first occurance of the contact in your friends list.
-     * @param searchName, the email of the contact to be searched in the friends list.
+     * @param email the email of the contact to be searched in the
+     * friends list.
      * @return the contact of the friend, null otherwise.
      */
-    public Contact searchContactByEmail(String email) {
+    public Contact searchContactByEmail(final String email) {
         //  Your code goes here....
-        for (int i=0; i< friendsCount; i++){
-            if (myFriends[i].getEmail().equals(email)){
+        for (int i = 0; i < friendsCount; i++) {
+            if (myFriends[i].getEmail().equals(email)) {
                 return myFriends[i];
             }
         }
@@ -83,18 +84,21 @@ class ContactsManager {
     }
 
     /**
-     * This method deletes the contact based on the name of your friend and returns
-     * true on success and false if failure in deleting the contact.
-     * 
-     * @param searchName, the name of the contact to be searched in the friends list.
-     * @return true if contact has been removed successfully and false otherwise.
+     * This method deletes the contact based on the name of your friend
+     * and returns true on success and false if failure in deleting the
+     *  contact.
+     *
+     * @param searchName the name of the contact to be searched in the
+     * friends list.
+     * @return true if contact has been removed successfully and false
+     * otherwise.
      */
-    public boolean deleteContact(String searchName) {
+    public boolean deleteContact(final String searchName) {
         //  Your code goes here....
-        for (int i=0; i < friendsCount; i++){
-            if (myFriends[i].getName().equals(searchName)){
-                for (int j = i; j < friendsCount-1; j++){
-                    myFriends[j] = myFriends[j+1];
+        for (int i = 0; i < friendsCount; i++) {
+            if (myFriends[i].getName().equals(searchName)) {
+                for (int j = i; j < friendsCount - 1; j++) {
+                    myFriends[j] = myFriends[j + 1];
                 }
                 friendsCount--;
                 myFriends[friendsCount] = null;
